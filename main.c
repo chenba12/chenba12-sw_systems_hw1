279f3eb0a630d546d6e6425b1d3d41cd6e29c35a
Chen Ben Ami <chen24201@gmail.com>
2022-11-22 09:44:19 +0200

    Update main.c

--- main.c
+++ main.c
@@ -5,25 +5,24 @@ int main() {
     int num1, num2;
     scanf("%d", &num1);
     scanf("%d", &num2);
-    
-    printf("The Armstrong numbers are: ");
+    printf("The Armstrong numbers are:");
     for (int i = num1; i <= num2; i++) {
-        if (isArmStrong(i)) printf("%d ", i);
+        if (isArmStrong(i)) printf(" %d", i);
     }
     printf("\n");
-    printf("The Palindromes are: ");
+    printf("The Palindromes are:");
     for (int i = num1; i <= num2; i++) {
-        if (isPalindrome(i)) printf("%d ", i);
+        if (isPalindrome(i)) printf(" %d", i);
     }
     printf("\n");
-    printf("The Prime numbers are: ");
+    printf("The Prime numbers are:");
     for (int i = num1; i <= num2; i++) {
-        if (isPrime(i)) printf("%d ", i);
+        if (isPrime(i)) printf(" %d", i);
     }
     printf("\n");
-    printf("The Strong numbers are: ");
-    for (int i = num1; i <= num2; i++) {
-        if (isStrong(i)) printf("%d ", i);
+    printf("The Strong numbers are:");
+    for (int i = num1; i <=num2; i++) {
+        if (isStrong(i)) printf(" %d", i);
     }
     printf("\n");
     return 0;
